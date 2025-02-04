863fa4e2d710006107b21a0339a9f1a6180aa68d
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-02-04 18:48:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+function ManipulateData(){
+	let output=document.getElementById("output");
+	new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve([1,2,3,4]);
+		},3000)
+	}).then((firstData)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				let evenArr=firstData.filter((n)=>n%2==0);
+				output.textContent=evenArr.join(' ');
+				resolve(evenArr);
+			},1000)
+		})
+	}).then((secData)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				let newArr=secData.filter((n)=>n*2);
+				output.textContent=newArr.join(' ');
+				resolve(newArr);
+			},2000)
+		})
+	})
+}
+
+
+ManipulateData();
